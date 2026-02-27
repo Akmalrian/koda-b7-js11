96b0bf20f0d9f6717f3caf70c77c1a9c93f17909
Akmalrian <105763366+Akmalrian@users.noreply.github.com>
2026-02-27 21:45:26 +0700

    Update minitask-11.js

--- minitask-11.js
+++ minitask-11.js
@@ -1,22 +1,45 @@
-const sintaks = new Promise ((resolve, reject)=>{
-    const isRaining = false
-    if (isRaining){
-        return reject("Nama = ")
+const sintaks = new Promise((resolve, reject) => {
+    const namaTampil = false
+
+    if (namaTampil) {
+        return reject("Nama tidak bisa ditampilkan")
     }
-    return resolve("")
+
+    return resolve({
+        nama1: "John",
+        nama2: "Ed",
+        nama3: "Jane"
+    })
 })
 
-sintaks.
-then(value=> {
-    function greet(name){
+function greet(name) {
     console.log(`Nama = ${name}`)
-
 }
-    setTimeout(greet, 1500,"John"+value)
-    setTimeout(greet, 2000,"Ed"+value)
-    setTimeout(greet, 500,"Jane"+value)
-    
+
+sintaks
+.then(value => {
+    setTimeout(greet, 1500, value.nama1)
+    setTimeout(greet, 2000, value.nama2)
+    setTimeout(greet, 500, value.nama3)
 })
-.catch(reason => {
-    console.log(`hasil : ${reason}`)
-});
+.catch(error => {
+    console.log(error)
+})
+
+function greet(name) {
+    console.log(`Nama = ${name}`)
+}
+
+async function tampilNama() {
+    try {
+        const value = await sintaks
+
+        setTimeout(greet, 1500, value.nama1)
+        setTimeout(greet, 2000, value.nama2)
+        setTimeout(greet, 500, value.nama3)
+
+    } catch (error) {
+        console.log(error)
+    }
+}
+tampilNama()
